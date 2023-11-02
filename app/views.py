f4c1a987618c8267074df60fb0b2f227934052b7
Gerrysan10 <20460135@colima.tecnm.mx>
2023-11-02 17:41:49 -0600

    lista de opciones con datos de la base

--- app/views.py
+++ app/views.py
@@ -7,7 +7,25 @@ def index(request):
     return render(request, 'index.html')
 
 def registro(request):
-    return render(request, 'registro.html')
+    # Código para obtener temas de MongoDB
+    mongo_client = pymongo.MongoClient('mongodb://daniel:belicon@25.10.16.136:27017/cursos')
+    db_mongo = mongo_client['cursos']
+    collection_mongo = db_mongo['curso']
+    temas = [curso['Tema'] for curso in collection_mongo.find()]
+
+    # Código para obtener instructores de SQL Server
+    server = '25.59.146.27'
+    database = 'instructores'
+    username = 'baseinstructor'
+    password = '123'
+    connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+    conn = pyodbc.connect(connection_string)
+    cursor = conn.cursor()
+    cursor.execute("SELECT CONCAT(Nombre, ' ', Primer_Apellido, ' ', Segundo_Apellido) AS NombreCompleto FROM Catalogo_instructores")
+    instructores = cursor.fetchall()
+
+    return render(request, 'registro.html', {'temas': temas, 'instructores': instructores})
+
 
 def cursos(request):
     return render(request, 'cursos.html')
